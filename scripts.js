524799e9cf42ec1012e243b60503df41864e36f6
orphet2005 <143293236+orphet2005@users.noreply.github.com>
2025-05-31 18:06:23 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+  
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('OrStore World ready!');
+});
+
+
+
+  
+
+
+
+
+function showAboutUs() {
+    document.getElementById("content").innerHTML = `
+      <h2>À propos de nous</h2>
+      <p>OrStore est une entreprise dédiée au numérique, à l'innovation et à la qualité de service. 
+      Nous mettons en avant des produits modernes pour répondre à vos besoins technologiques.</p>
+    `;
+  }
+
+  function showNews() {
+    document.getElementById("content").innerHTML = `
+      <h2>Actualités</h2>
+      <p>📢 Nouvelle collection de produits high-tech disponible dès maintenant !</p>
+    `;
+  }
+
+
+
+
+
+
+
+  
